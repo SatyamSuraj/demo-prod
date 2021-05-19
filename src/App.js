797c1f199a37cb25c15bcbe2ac571272a382484e
missile786 <satyamsurajmaurya@gmail.com>
2021-05-20 00:26:18 +0530

    Collection item and collection preview component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,9 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
 
-const HatsPage = () => (
-  <div>
-    HATS PAGE
-  </div>
-);
+
 
 function App() {
   return (
@@ -15,7 +12,7 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route exact path='/' component={HomePage}/>
-          <Route path='/shop/hats' component={HatsPage}/>
+          <Route path='/shop' component={ShopPage}/>
         </Switch>
       </BrowserRouter>
     </div>
